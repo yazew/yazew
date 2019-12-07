ca0fdb55453fadaa82cd8db943ac328a0f1ebb9d
Yared Getachew <veduket@gmail.com>
2019-12-07 18:58:23 +0300

    added process.env.PORT

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
 const config = require('dotenv').config().parsed
+const express = require('express')
+const PORT = process.env.PORT || 5000
 const api = require('./api')
 const webapp = require('./webapp')
 const server = express()
 server.use('/api/',api)
 server.use('/',webapp)
-const PORT = config.PORT || 5000
 server.listen(PORT, () => {
   console.log(`Server started to listen to PORT : ${PORT}`)
 })
